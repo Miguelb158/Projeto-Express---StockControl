5aab4df0f9618cc7fbeb112e2534b4db49a72dc8
miguel_borges <miguelborgess2016@gmail.com>
2025-10-07 08:59:50 -0300

    /

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/login.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login - Móveis Armazenamento</title>
+  <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <form action="">
-        <h1>oi</h1>
-    </form>
+  <div class="container">
+    <div class="left-panel">
+      <img id="logo" src="./img/logo.png" alt="Logo" class="logo">
+      <h2>Seja bem-vindo</h2>
+      <p>Ainda não tem uma conta?<br>Clique aqui embaixo e cadastre-se</p>
+      <button class="btn-voltar">Voltar</button>
+    </div>
+    <div class="right-panel">
+      <h2>Login</h2>
+      <div class="social-icons">
+        <button>f</button>
+        <button>G+</button>
+        <button>in</button>
+      </div>
+      <form>
+        <input type="text" placeholder="Name">
+        <input type="email" placeholder="Email">
+        <input type="password" placeholder="Senha">
+        <button class="btn-cadastrar">Cadastrar-se</button>
+      </form>
+    </div>
+  </div>
 </body>
 </html>
