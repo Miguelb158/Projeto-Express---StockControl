a20cc8a2394f2eeb4e7d93bf1f0eb0a88c8bee75
miguel_borges <miguelborgess2016@gmail.com>
2025-10-08 00:36:52 -0300

    ;)

--- inicio.php
+++ inicio.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Página inicial</title>
+    <link rel="stylesheet" href="./css/inicio.css">
 </head>
 <body>
-    <h1>oi</h1>
+    <div class="container">
+        <nav class="sidebar">
+            <DIv id="DIVELOGO">
+
+                <img id="logo" src="./img/logo.png" alt="Logo Móveis Armazenamento">
+                
+                <ul class="menu">
+                    <li><a href="#"><img class="icone" src="./img/casa.png" alt=""> Início</a></li>
+                    <li><a href="#"><img class="icone" src="./img/caixa-aberta.png" alt=""> Estoque</a></li>
+                    <li><a href="#"><img class="icone" src="./img/do-utilizador.png" alt=""> Novo usuário</a></li>
+                    <li><a href="#"><img class="icone" src="./img/prancheta.png" alt=""> Histórico</a></li>
+                </ul>
+                
+            </DIv>
+            <a id="sair" href="index.php">< Sair</a>
+        </nav>
+
+        <main class="conteudo">
+            <h1>Página inicial</h1>
+            <div class="grafico">
+                <!-- Aqui vai o gráfico (por exemplo, canvas ou imagem) -->
+                <img src="./img/grafico-exemplo.png" alt="Gráfico" style="width: 400px;">
+            </div>
+        </main>
+    </div>
 </body>
 </html>
